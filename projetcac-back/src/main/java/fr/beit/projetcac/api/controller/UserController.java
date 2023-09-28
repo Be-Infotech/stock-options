140862602d89d0ffb48f632d2a090181b172dbc9
lheniart <lheniart@be-infotech.fr>
2023-09-28 20:37:05 +0200

    feat(git) create UserService

---
+++ projetcac-back/src/main/java/fr/beit/projetcac/api/controller/UserController.java
@@ -0,0 +1,17 @@
+package fr.beit.projetcac.api.controller;
+
+import fr.beit.projetcac.api.model.User;
+import fr.beit.projetcac.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUsers")
+    public Iterable<User>getUsers(){return userService.getUsers(); }
+}
