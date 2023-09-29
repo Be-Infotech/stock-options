121f3c77da69f3d5e66ef9bfa6f1dd9cf37313ab
Lheniart <146065771+Lheniart@users.noreply.github.com>
2023-09-29 11:18:12 +0200

    Update index.js

--- projetcac-front/src/router/index.js
+++ projetcac-front/src/router/index.js
@@ -8,11 +8,11 @@ const routes = [
     children: [
       {
         path: '',
-        name: 'Loggin',
+        name: 'Login',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Loggin.vue'),
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Login.vue'),
       },
     ],
   },
