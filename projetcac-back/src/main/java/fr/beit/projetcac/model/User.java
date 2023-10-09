17d381c8df740a37335a40e08be465bec74c06c3
LoanH <lheniart@be-infotech.fr>
2023-10-09 11:03:17 +0200

    feat(git): add User model

---
+++ projetcac-back/src/main/java/fr/beit/projetcac/model/User.java
@@ -0,0 +1,49 @@
+package fr.beit.projetcac.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"username"}),
+        @UniqueConstraint(columnNames = {"mail"})
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "profile_photo")
+    private String profilePhoto;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "postalCode")
+    private String postalCode;
+
+}
