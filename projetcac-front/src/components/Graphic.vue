86b821c7e0306d87e0b3666dd287c877ded3a5f1
LoanH <lheniart@be-infotech.fr>
2023-10-09 17:37:18 +0200

    feat(git): add dashboard

---
+++ projetcac-front/src/components/Graphic.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: "Graphic",
+  props: {
+    title : String
+  },
+}
+</script>
+
+<template>
+<div id="componentWrapper">
+  <div id="graphicWrapper"></div>
+  <p id="pTitle">{{title}}</p>
+</div>
+</template>
+
+<style scoped lang="scss">
+#componentWrapper{
+  background-color: white;
+  width: 430px;
+  height: 300px;
+  border-radius: 17px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 5px 5px 15px #343434;
+}
+#graphicWrapper{
+  background-color: #00CAE3;
+  width: 404px;
+  Height: 185px;
+  border-radius: 10px;
+  transform: translateY(-20px);
+}
+#pTitle{
+  font-weight: 600;
+  font-size: 32px;
+  font-family: Roboto, sans-serif;
+  line-height: 37.5px;
+}
+</style>
---
+++ projetcac-front/src/views/Dashboard.vue
@@ -0,0 +1,48 @@
+<style scoped lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#pagewrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+#graphWrapper, #tabWrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 40px 10px;
+}
+
+</style>
+
+<template>
+  <Head/>
+  <div id="pagewrapper">
+    <div id="graphWrapper">
+      <Graphic title="Weekly evolution"/>
+      <Graphic title="Monthly evolution"/>
+      <Graphic title="Semi-annual evolution"/>
+
+    </div>
+    <div id="tabWrapper"></div>
+  </div>
+</template>
+<script>
+import Head from "@/components/Head.vue";
+import Graphic from "@/components/Graphic.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    Head,
+    Graphic
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
