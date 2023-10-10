22b129392cd90e5ed2731f587ddf6a79b54a43f2
LoanH <lheniart@be-infotech.fr>
2023-10-10 14:20:16 +0200

    feat(git): add test

---
+++ projetcac-front/vitest.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      transformMode: {
+        web: [/\.[jt]sx$/]
+      },
+      setupFiles: "./src/vitest.setup.ts",
+      coverage: {
+        provider: 'v8',
+        reporter: ['html', 'lcov']
+      },
+      sequence: {
+        shuffle: true,
+      },
+    }
+  })
+)
