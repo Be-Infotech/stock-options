05c163aed6b4f2c6a5b9280311f01d6f8d719f26
LoanH <lheniart@be-infotech.fr>
2023-10-17 16:07:06 +0200

    refactor(git) refactor User and SecurityConfig

--- projetcac-back/src/main/java/fr/beit/projetcac/user/UserService.java
+++ projetcac-back/src/main/java/fr/beit/projetcac/user/UserService.java
@@ -1,10 +1,6 @@
 package fr.beit.projetcac.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,58 +13,50 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
 
-    //Pas authentifié
-    //Authentifié et connu en base
 
     public Optional<User> authenticateUser(String username, String password) {
+        Optional<User> user = userRepository.findByUsernameOrMail(username, username);
 
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                username, password
-        ));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        if (authentication.isAuthenticated()) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            return userRepository.findByUsernameOrMail(userDetails.getUsername(), userDetails.getUsername())
-                    .map(user -> new User(
-                            user.getId(),
-                            user.getUsername(),
-                            "",
-                            user.getMail(),
-                            user.getFirstName(),
-                            user.getLastName(),
-                            user.getProfilePhoto(),
-                            user.getCity(),
-                            user.getAddress(),
-                            user.getCountry(),
-                            user.getPostalCode()
-                    ));
-        }
-        else
+        if (user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())) {
+           User userInfo = new User(
+                    user.get().getId(),
+                    user.get().getUsername(),
+                    "####",
+                    user.get().getMail(),
+                    user.get().getFirstName(),
+                    user.get().getLastName(),
+                    user.get().getProfilePhoto(),
+                    user.get().getCity(),
+                    user.get().getAddress(),
+                    user.get().getCountry(),
+                    user.get().getPostalCode()
+            );
+           return Optional.of(userInfo);
+        } else{
             return Optional.empty();
+        }
     }
 
 
-    public Mono<String> savePassword(String usernameOrMail){
-       return Mono.justOrEmpty(userRepository.findByUsernameOrMail(usernameOrMail,usernameOrMail))
-               .map(user -> new User(
-                       user.getId(),
-                       user.getUsername(),
-                       passwordEncoder.encode("1234"),
-                       user.getMail(),
-                       user.getFirstName(),
-                       user.getLastName(),
-                       user.getProfilePhoto(),
-                       user.getCity(),
-                       user.getAddress(),
-                       user.getCountry(),
-                       user.getPostalCode()
-               ))
-               .flatMap(user -> Mono.justOrEmpty(userRepository.save(user)))
-               .map(User::getPassword);
+    public Mono<String> savePassword(String usernameOrMail) {
+        return Mono.justOrEmpty(userRepository.findByUsernameOrMail(usernameOrMail, usernameOrMail))
+                .map(user -> new User(
+                        user.getId(),
+                        user.getUsername(),
+                        passwordEncoder.encode("1234"),
+                        user.getMail(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getProfilePhoto(),
+                        user.getCity(),
+                        user.getAddress(),
+                        user.getCountry(),
+                        user.getPostalCode()
+                ))
+                .flatMap(user -> Mono.justOrEmpty(userRepository.save(user)))
+                .map(User::getPassword);
     }
+
 }
