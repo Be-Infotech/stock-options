5433209bf7e5d72d66401756d7f713486b642c56
LoanH <lheniart@be-infotech.fr>
2023-10-17 10:17:00 +0200

    feat(git) add Test Service

--- projetcac-back/src/main/java/fr/beit/projetcac/config/SecurityConfig.java
+++ projetcac-back/src/main/java/fr/beit/projetcac/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package fr.beit.projetcac.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -28,6 +36,21 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeHttpRequests((authorize) ->
+                        //authorize.anyRequest().authenticated()
+                        authorize.requestMatchers(HttpMethod.GET, "/**").permitAll()
+                                .requestMatchers("/user/**").permitAll()
+                                .anyRequest().authenticated()
+
+                );
+
+        return http.build();
     }
 
 }
--- projetcac-back/src/test/java/fr/beit/projetcac/usertest/UserControllerTest.java
+++ projetcac-back/src/test/java/fr/beit/projetcac/usertest/UserControllerTest.java
@@ -8,25 +8,37 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.Optional;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+
 @WebFluxTest(UserController.class)
+@Import(UserControllerTest.TestConfig.class)
 public class UserControllerTest {
+
 
     @Autowired
     WebTestClient webTestClient;
-
     @MockBean
     UserService userService;
 
+    static class TestConfig {
+        @Bean
+        SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+            return http.csrf(ServerHttpSecurity.CsrfSpec::disable).build();
+        }
+    }
+
     @Nested
-    class authenticateUser{
+    class authenticateUser {
 
         //Bien authentifié
         //Pas authentifié
@@ -34,14 +46,14 @@ public class UserControllerTest {
 
 
         @Test
-        void shouldReturnUser(){
+        void shouldReturnUser() {
             when(userService.authenticateUser("toto", "thisisaspassword"))
                     .thenReturn(Optional.of(new User(
                             1,
                             "toto",
                             "1234",
-                            "test@mail.com",
-                            "",
+                            "toto@mail.com",
+                            "toto",
                             "",
                             "",
                             "",
@@ -55,21 +67,28 @@ public class UserControllerTest {
                     .contentType(APPLICATION_JSON)
                     .body(fromValue("""
                             {
-                            "usernameOrMail" : "toto"
-                            "password" : "1234"
+                                "usernameOrMail" : "toto"
+                                "password" : "1234"
                             }
                             """))
                     .exchange()
                     .expectStatus().isOk()
                     .expectBody()
-                    .json( """
-                        {
-                            "id": 1,
-                            "username": "toto"
-                            "password": "",
-                            
-                        }
-                    """
+                    .json("""
+                                {
+                                    "id": 1,
+                                    "username": "toto",
+                                    "password": "",
+                                    "mail": "toto@mail.com",
+                                    "firstName": "toto",
+                                    "lastName": "toto",
+                                    "profilePhoto": "",
+                                    "city": "",
+                                    "address": "",
+                                    "country": "",
+                                    "postalCode": "" ,
+                                }
+                            """
                     );
         }
     }
