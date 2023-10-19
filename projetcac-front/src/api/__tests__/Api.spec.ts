3c77482cd6eebd4ab7f35ea7e517ad3241b72a24
lheniart <lheniart@be-infotech.fr>
2023-10-19 18:31:11 +0200

    feat(front) add api.spec.ts

---
+++ projetcac-front/src/api/__tests__/Api.spec.ts
@@ -0,0 +1,40 @@
+import {describe, expect, it,beforeEach,afterEach} from 'vitest';
+import nock from 'nock';
+import {loginApi, User} from "../api";
+import {post} from "axios";
+
+describe('loginApi', () => {
+    const username = 'testuser';
+    const password = 'testpassword';
+    const url = 'http://localhost:8080';
+
+    afterEach(() => {
+        nock.cleanAll();
+    });
+
+    it('Should return code 200 and User', async () => {
+        // Réponse simulée que vous souhaitez renvoyer
+        const responseData: User = {
+            username: "toto",
+            password: "####",
+            firstName: "",
+            lastName: "",
+            mail: "toto@mail.com",
+            address: "",
+            city: "",
+            country: '',
+            postalCode: "",
+            profilePhoto: ""
+        }
+
+
+        nock(url)
+            .post('/user/signin')
+            .reply(200, responseData);
+
+        const result = await loginApi(username, password);
+        expect(result).toEqual(responseData);
+    });
+
+
+});
