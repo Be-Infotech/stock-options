b89f5b101fd07d0bf6aa0f05300afdd31ef0b7f1
LoanH <lheniart@be-infotech.fr>
2023-10-20 12:14:22 +0200

    fix(back) fix User

--- projetcac-back/src/main/java/fr/beit/projetcac/user/UserService.java
+++ projetcac-back/src/main/java/fr/beit/projetcac/user/UserService.java
@@ -1,8 +1,6 @@
 package fr.beit.projetcac.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
