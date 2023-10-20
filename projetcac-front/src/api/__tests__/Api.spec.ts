f5b3afc3957b028bc2c4a1a76d7bd75a31c5b2b2
Dahodges <antoine.gesquiere@outlook.com>
2023-10-20 10:23:18 +0200

    Fix

--- projetcac-front/src/api/__tests__/Api.spec.ts
+++ projetcac-front/src/api/__tests__/Api.spec.ts
@@ -1,55 +1,63 @@
-import {describe, expect, it,beforeEach,afterEach, } from 'vitest';
+import {describe, expect, it, beforeEach, afterEach,} from 'vitest';
 import nock from 'nock';
 import {loginApi, User} from "../api";
 import axios, {isAxiosError} from "axios";
 
 describe('loginApi', () => {
-    const username = 'testuser';
-    const password = 'testpassword';
-    const url = 'http://localhost:8080';
+  const username = 'testuser';
+  const password = 'testpassword';
+  const url = 'http://localhost:8080';
 
-    afterEach(() => {
-        nock.cleanAll();
-    });
+  afterEach(() => {
+    nock.cleanAll();
+  });
 
-    beforeEach(() => {
-      // fix: Cross origin http://localhost forbidden)
-      axios.defaults.adapter = 'http';
-    });
+  beforeEach(() => {
+    // fix: Cross origin http://localhost forbidden)
+    axios.defaults.adapter = 'http';
+  });
 
-    it('Should return code 200 and User', async () => {
-        // Réponse simulée que vous souhaitez renvoyer
-        const responseData: User = {
-            username: "toto",
-            password: "####",
-            firstName: "",
-            lastName: "",
-            mail: "toto@mail.com",
-            address: "",
-            city: "",
-            country: '',
-            postalCode: "",
-            profilePhoto: ""
-        }
+  it('Should return code 200 and User', async () => {
+    // Réponse simulée que vous souhaitez renvoyer
+    const responseData: User = {
+      username: "toto",
+      password: "####",
+      firstName: "",
+      lastName: "",
+      mail: "toto@mail.com",
+      address: "",
+      city: "",
+      country: '',
+      postalCode: "",
+      profilePhoto: ""
+    }
 
 
-        nock(url)
-            .post('/user/signin')
-            .reply(200, responseData);
+    nock(url)
+      .post('/user/signin')
+      .reply(200, responseData);
 
-        const result = await loginApi(username, password);
-        expect(result).toEqual(responseData);
-    });
+    const result = await loginApi(username, password);
+    expect(result).toEqual(responseData);
+  });
+
+
+  expect.extend({
+
+    verify<T>(received: any, predicate: (value: T) => boolean) {
+      return {pass: predicate(received), message: () => `expected ${received} to match ${predicate}`};
+    },
+  });
+
   it('Should return code 401', async () => {
     // Réponse simulée que vous souhaitez renvoyer
-
 
 
     nock(url)
       .post('/user/signin')
       .reply(404);
     await expect(loginApi(username, password)).rejects.toThrow(
-      expect.verify(x => isAxiosError(x) && x.response?.status === 404));
+      expect.verify(x => isAxiosError(x) && x.response?.status === 404));
   });
 
 
