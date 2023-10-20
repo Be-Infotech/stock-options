914aa0842219cfb39ecd575d2db6ca7f2b6ff069
LoanH <lheniart@be-infotech.fr>
2023-10-20 16:02:13 +0200

    feat(back) add user update data in back

--- projetcac-back/src/main/java/fr/beit/projetcac/user/UserController.java
+++ projetcac-back/src/main/java/fr/beit/projetcac/user/UserController.java
@@ -3,10 +3,7 @@ package fr.beit.projetcac.user;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
@@ -30,6 +27,12 @@ public class UserController {
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR)));
     }
 
+    @PostMapping("/updateData")
+    public Mono<User> getUserInfo(@RequestBody UserInfoDto userInfoDto){
+        return Mono.justOrEmpty(userService.updateUserInfo(userInfoDto))
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED)));
+    }
+
     @Value
     public static class LoginDto {
         String usernameOrMail;
@@ -38,6 +41,19 @@ public class UserController {
     @Value
     public static class ResetPasswordDto {
         String usernameOrMail;
+    }
+
+    @Value
+    public static class UserInfoDto{
+        String username;
+        String mail;
+        String profilePhoto;
+        String firstName;
+        String lastName;
+        String address;
+        String city;
+        String country;
+        String postalCode;
     }
 }
 
